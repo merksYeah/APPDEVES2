88e9832e33688d86461e83376988ce96b35a9d79
MiguelMercado <miguelangelomercado@gmail.com>
2015-04-10 22:43:27 +0800

    YEAH

--- CRIPISI/CRIPISI/src/java/CreateSalesOrder.java
+++ CRIPISI/CRIPISI/src/java/CreateSalesOrder.java
@@ -19,6 +19,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 /**
  *
  * @author deathman28
@@ -67,7 +68,6 @@ public class CreateSalesOrder extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try {
@@ -85,7 +85,6 @@ public class CreateSalesOrder extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try {
@@ -100,7 +99,6 @@ public class CreateSalesOrder extends HttpServlet {
      *
      * @return a String containing servlet description
      */
-    @Override
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
